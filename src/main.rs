e0cb38d4cabcd82d605ac60df8e2b7c5f82b87d8
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-04-22 01:33:22 +0200

    feat: Use app lib

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,20 @@
-use listeners::{Listener, UnknownListener};
-use resources::Resource;
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
-use views::{UnknownView, View};
-
+use lenra_app::{manifest::Manifest, LenraApp, resource::map_resources};
+use resources::RESOURCE_MAP;
 mod data;
 mod listeners;
 mod resources;
 mod views;
 
 fn main() {
-    env_logger::init();
+    let app = LenraApp {
+        manifest: Manifest {
+            root_view: "main".into(),
+        },
+        views: vec![],
+        listeners: vec![],
+        resources: map_resources(RESOURCE_MAP),
+        ..Default::default()
+    };
 
-    let body = serde_json::from_reader(std::io::stdin());
-    if let Ok(request) = body {
-        match request {
-            Request::View(view) => print!("{}", view.handle()),
-            Request::Listener(listener) => listener.handle(),
-            Request::NotManagedView(v) => {
-                log::error!("Not managed view '{}'", v.view);
-                panic!("Unknown view {}", v.view)
-            }
-            Request::NotManagedListener(l) => {
-                log::warn!("Not managed listener '{:?}'", l);
-                panic!("Unknown action {}", l.action)
-            }
-            Request::Resource(resource) => resource.handle(),
-            Request::Other(_) => print!("{}", handle_manifest()),
-        }
-    } else {
-        print!("{}", handle_manifest());
-    }
-}
-
-/** The application input */
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-#[serde(untagged)]
-pub enum Request {
-    Listener(Listener),
-    NotManagedListener(UnknownListener),
-    View(View),
-    NotManagedView(UnknownView),
-    Resource(Resource),
-    Other(Value),
-}
-
-fn handle_manifest() -> Value {
-    json!({
-        "manifest": {
-            "rootView": "main"
-        }
-    })
+    app.run().unwrap();
 }
--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,10 +1,3 @@
-use std::{
-    collections::HashMap,
-    io::{self, Write},
-};
-
-use serde::{Deserialize, Serialize};
-
 macro_rules! resource {
     ($path: tt) => {
         ($path, include_bytes!($path))
@@ -12,18 +5,3 @@ macro_rules! resource {
 }
 
 pub const RESOURCE_MAP: [(&str, &[u8]); 1] = [resource!("logo.png")];
-
-/** Lenra view request */
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct Resource {
-    pub resource: String,
-}
-
-impl Resource {
-    pub fn handle(&self) {
-        let map: HashMap<&str, &[u8]> = HashMap::from(RESOURCE_MAP);
-        io::stdout()
-            .write_all(map.get(self.resource.as_str()).unwrap())
-            .unwrap();
-    }
-}
